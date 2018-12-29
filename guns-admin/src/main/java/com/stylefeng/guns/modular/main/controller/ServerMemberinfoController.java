cd59930b44fa26ae6658f78d14bb20d291f92dde
chenyq <1418806370@qq.com>
2018-12-29 10:17:36 +0800

    增值服务

--- guns-admin/src/main/java/com/stylefeng/guns/modular/main/controller/ServerMemberinfoController.java
+++ guns-admin/src/main/java/com/stylefeng/guns/modular/main/controller/ServerMemberinfoController.java
@@ -1,10 +1,15 @@
 package com.stylefeng.guns.modular.main.controller;
 
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.stylefeng.guns.core.base.controller.BaseController;
+import com.stylefeng.guns.modular.main.service.IMembermanagementService;
+import com.stylefeng.guns.modular.main.service.IMembershipcardtypeService;
+import com.stylefeng.guns.modular.system.model.*;
 import org.springframework.stereotype.Controller;
 import com.baomidou.mybatisplus.plugins.Page;
 import com.stylefeng.guns.core.common.constant.factory.PageFactory;
 import com.stylefeng.guns.core.common.BaseEntityWrapper.BaseEntityWrapper;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.ui.Model;
@@ -12,8 +17,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.stylefeng.guns.core.log.LogObjectHolder;
 import org.springframework.web.bind.annotation.RequestParam;
-import com.stylefeng.guns.modular.system.model.ServerMemberinfo;
 import com.stylefeng.guns.modular.main.service.IServerMemberinfoService;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * 体验服务记录控制器
@@ -29,6 +37,10 @@ public class ServerMemberinfoController extends BaseController {
 
     @Autowired
     private IServerMemberinfoService serverMemberinfoService;
+    @Autowired
+    private IMembermanagementService membermanagementService;
+    @Autowired
+    private IMembershipcardtypeService membershipcardtypeService;
 
     /**
      * 跳转到体验服务记录首页
@@ -65,6 +77,8 @@ public class ServerMemberinfoController extends BaseController {
     public Object list(String condition) {
         Page<ServerMemberinfo> page = new PageFactory<ServerMemberinfo>().defaultPage();
         BaseEntityWrapper<ServerMemberinfo> baseEntityWrapper = new BaseEntityWrapper<>();
+        if(!StringUtils.isEmpty(condition))baseEntityWrapper.like("memberName",condition);
+        baseEntityWrapper.orderBy("createTime",false);
         Page<ServerMemberinfo> result = serverMemberinfoService.selectPage(page, baseEntityWrapper);
         return super.packForBT(result);
     }
@@ -106,5 +120,22 @@ public class ServerMemberinfoController extends BaseController {
     @ResponseBody
     public Object detail(@PathVariable("serverMemberinfoId") Integer serverMemberinfoId) {
         return serverMemberinfoService.selectById(serverMemberinfoId);
+    }
+
+    @RequestMapping("getMemberInfo")
+    @ResponseBody
+    public Object selectMemberInfo(Integer memberId) throws ParseException {
+        Membermanagement m = membermanagementService.selectById(memberId);
+        Membershipcardtype ms = membershipcardtypeService.selectById(m.getLevelID());
+        Map<String, Object> memberinfo = new HashMap<>();
+        memberinfo.put("cadID", m.getCadID());
+        memberinfo.put("name", m.getName());
+        memberinfo.put("id", m.getId());
+        memberinfo.put("phone", m.getPhone());
+        memberinfo.put("address", m.getAddress());
+        memberinfo.put("countPrice", m.getCountPrice());
+        memberinfo.put("levelID", ms.getCardname());
+        memberinfo.put("integral", m.getIntegral());
+        return memberinfo;
     }
 }
